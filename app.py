12ccc8548c65a1776d38c78db76c9d094dbc5829
Phantom-VK <vikramadityakhupse@gmail.com>
2025-08-25 21:28:17 +0530

    Added middleware and more metrics

--- app.py
+++ app.py
@@ -1,24 +1,44 @@
+import time
+
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 import random
 from prometheus_client import  make_asgi_app
-from config.prometheus_metrics import RANDOM_NUMBER_GAUGE, REQUEST_COUNTER
+from prometheus.prometheus_metrics import RANDOM_NUMBER_GAUGE, REQUEST_COUNTER, REQUEST_STATUS_COUNTER, \
+    REQUEST_LATENCY_HISTOGRAM
 
 app = FastAPI()
 metrics_app = make_asgi_app()
 app.mount("/metrics", app = metrics_app)
 
+@app.middleware("http")
+async def metrics_middleware(request:Request, call_next):
+    start_time = time.time()
+
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    endpoint = request.url.path
+    status_code = str(response.status_code)
+    method = request.method
+
+    REQUEST_COUNTER.labels(endpoint = endpoint).inc()
+    REQUEST_STATUS_COUNTER.labels(endpoint = endpoint, status_code = status_code).inc()
+    REQUEST_LATENCY_HISTOGRAM.labels(endpoint = endpoint, method = method).observe(process_time)
+    return response
+
 @app.get("/", response_class=JSONResponse)
 def get_homepage():
-    # Increment request counter
-    REQUEST_COUNTER.labels(endpoint="/").inc()
+    random_num = random.randint(1, 20)
 
-    random_num = random.randint(1, 10)
-
-    # Set random number gauge
     RANDOM_NUMBER_GAUGE.set(random_num)
-    return {
+
+    if random_num % 2 == 0:
+        return {
         "status":"OK",
+        "random_number":random_num
+    }
+    return {
+        "status":"ERROR",
         "random_number":random_num
     }
 
---
+++ prometheus/prometheus_metrics.py
@@ -0,0 +1,25 @@
+from prometheus_client import Counter, Gauge, Histogram
+
+REQUEST_COUNTER = Counter(
+    name="app_requests_total",
+    documentation="Total number of requests to the app",
+    labelnames=["endpoint"]
+)
+
+RANDOM_NUMBER_GAUGE = Gauge(
+    name="app_random_number",
+    documentation="Current value of the random number."
+)
+REQUEST_STATUS_COUNTER = Counter(
+    name="request_status_counter",
+    documentation="Total number of responses by status code",
+    labelnames=['endpoint', 'status_code']
+)
+
+REQUEST_LATENCY_HISTOGRAM = Histogram(
+    name="request_latency_histogram",
+    documentation="The latency of requests in seconds",
+    labelnames=['endpoint', 'method']
+
+)
+
